083e512427af60c51f12617b0392614438a1a7a8
lixiaowei <267199@qq.com>
2024-07-14 18:01:42 +0800

    clr

--- mt7628nnRTK.sh
+++ mt7628nnRTK.sh
@@ -14,7 +14,7 @@ sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config
 #PASSWORD_HASH=$(openssl passwd -1 "admin")
 #sed -i "s|root::0:0:99999:7:::|root:${PASSWORD_HASH}:18297:0:99999:7:::|g" openwrt/package/base-files/files/etc/shadow
 
-#2. Clear the login password
+#2. Clear the login password。
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 # 3. Configure Ntrip services
